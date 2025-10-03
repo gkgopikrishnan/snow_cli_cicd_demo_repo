23efc34d34252e869f03645f833f0d6e5585e7a8
Gopikrishnan G <gkgopikrishnan@gmail.com>
2025-10-03 21:44:08 +0530

    New Chnage View

--- deploy/main_deploy.sql
+++ deploy/main_deploy.sql
@@ -1,8 +1,5 @@
 !source scripts/tables/employees.sql
 !source scripts/tables/departments.sql
-<<<<<<< HEAD
 !source scripts/tables/create_views.sql
-=======
 !source scripts/tables/vw_emp.sql
 
->>>>>>> fe910178a781d949c71db2fddce721268db2d578
